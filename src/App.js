8404b422551592c157621abf832771873e052d03
Dave <daveymason@outlook.com>
2021-04-05 09:48:02 +0800

    Update App.js
    get data from the API and useEffect to see what data we get in the console.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { useEffect} from "react";
 import './App.css';
+
 
 const App = () => { 
   //Usestate variables
-  const [pokemon,setPokemon] = useState("pikachu");
-  const [pokemonData, setPokemonDate] = useState([]);
-  const [pokemonType, setPokemonType] = useState([]);
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+
+  //Get data from API
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
 
   return (
     <div className="App">
