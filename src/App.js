552553cf92b8101ad97b2f30bffbee8d81149efc
Dave <daveymason@outlook.com>
2021-04-05 09:34:00 +0800

    Update App.js
    useState variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
-const App = () => {
+const App = () => { 
+  //Usestate variables
+  const [pokemon,setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonDate] = useState([]);
+  const [pokemonType, setPokemonType] = useState([]);
+
   return (
     <div className="App">
      <h1>Hello</h1>
