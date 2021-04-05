1dd414ad9f515a5008b620cfb344e44ae8d10f2b
Dave <daveymason@outlook.com>
2021-04-05 10:15:36 +0800

    Added Images and CSS
    Styled the page

--- src/App.js
+++ src/App.js
@@ -16,21 +16,73 @@ const App = () => {
     try {
       const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
       const res = await axios.get(url);
+
+      //Set useStates to data
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name);
+      setPokemonData(toArray);
+
       console.log(res);
     } catch (e) {
       console.log(e);
     }
   };
 
-  useEffect(() => {
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase())
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     getPokemon();
-  }, []);
+  }
 
   return (
     <div className="App">
-     <h1>Hello</h1>
-    </div>
-  );
-}
+     <form onSubmit={handleSubmit}>
+       <label>
+         <input type="text" 
+         onChange={handleChange} 
+         placeholder="Enter Pokemon"
+         />
+       </label>
+     </form>
+
+     {pokemonData.map((data) => {
+      return (
+      <div className="container">
+        <img src={data.sprites["front_default"]} />
+        <div className="divTable">
+          <div className="divTableBody">
+            <div className="divTableRow">
+              <div className="divTableCell">Type</div>
+              <div className="divTableCell">{pokemonType}</div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">Height</div>
+              <div className="divTableCell">
+                {" "}
+                {Math.round(data.height * 3.9)}"
+              </div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">Weight</div>
+              <div className="divTableCell">
+                {" "}
+                {Math.round(data.weight / 4.3)} lbs
+              </div>
+            </div>
+            <div className="divTableRow">
+              <div className="divTableCell">Number of Battles</div>
+              <div className="divTableCell">{data.game_indices.length}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  })}
+</div>
+);
+};
 
 export default App;
