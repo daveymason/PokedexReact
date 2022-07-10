1ca268690580b1b042ee5ef4cb6d9d5508933e0e
Davey Mason <daveymason@outlook.com>
2022-07-10 15:07:13 +0200

    Fixed AppBar

--- src/Components/Pokemon.jsx
+++ src/Components/Pokemon.jsx
@@ -2,18 +2,23 @@ import axios from "axios";
 import { useState } from "react";
 import * as React from "react";
 import DataGrid, {
-  Scrolling,  Column, 
-  Selection, FilterRow,
-} from 'devextreme-react/data-grid';
+  Scrolling,
+  Column,
+  Selection,
+  FilterRow,
+} from "devextreme-react/data-grid";
 
-
-import {PokemonDetails} from './PokemonDetails';
+import { PokemonDetails } from "./PokemonDetails";
 //https://js.devexpress.com/Demos/WidgetsGallery/Demo/DataGrid/RowSelection/React/Light/
 
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material/Send";
-
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import { textTransform } from "@mui/system";
 
 const Pokemon = () => {
   //Usestate variables
@@ -30,7 +35,6 @@ const Pokemon = () => {
       //Set useStates to data
       toArray.push(e.data);
       setPokemonData(toArray);
-
     } catch (e) {
       console.log(e);
     }
@@ -42,59 +46,64 @@ const Pokemon = () => {
       .then((url) => setpokemon({ pokemon: url.results }));
   };
 
+  window.onload = getAllPokemon;
+
   const handleChange = (e) => {
-    const pokemon = e.data.name; //to do: update to useEffect 
+    const pokemon = e.data.name; //to do: update to useEffect
     getPokemon(pokemon);
   };
 
+  const drawerWidth = 240;
+
   return (
-    <Grid container justifyContent="space-evenly" direction="row" className="App">
-      <Grid
-        item
-        xs={12}
-      >
-        <Button
-          onClick={getAllPokemon}
-          variant="contained"
-          endIcon={<SendIcon />}
+    <Grid container justifyContent="flex-end" direction="row" className="App">
+      <Box sx={{ display: "flex", textTransform: 'capitalize' }}>
+        <AppBar
+          position="fixed"
+          sx={{
+            width: `calc(100% - ${drawerWidth}px)`,
+            ml: `${drawerWidth}px`,
+          }}
+        />
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="permanent"
+          anchor="left"
         >
-          getAllPokemon
-        </Button>
-        </Grid>
-      <Grid
-        item
-        xs={2}
-      >
-        <DataGrid
-          dataSource={pokemon.pokemon}
-          rowAlternationEnabled={true}
-          showBorders={true}
-          height={'85vh'}
-          onRowClick={handleChange}
-          remoteOperations={true}
-        >
-          <Selection mode="single" />
-          <Column dataField="name" caption="Name" />
-          <Scrolling mode="virtual" rowRenderingMode="virtual" />
-          <FilterRow visible={true} />
-        </DataGrid>
-        <hr />
-      </Grid>
-    
-      
+          <Toolbar />
+          <Divider />
+          <DataGrid
+            dataSource={pokemon.pokemon}
+            rowAlternationEnabled={true}
+            showBorders={true}
+            onRowClick={handleChange}
+            remoteOperations={true}
+          >
+            <Selection mode="single" />
+            <Column dataField="name" caption="Name" />
+            <Scrolling mode="virtual" rowRenderingMode="virtual" />
+            <FilterRow visible={true} />
+          </DataGrid>
+        </Drawer>
+      </Box>
+  
         {pokemonData.map((data) => {
           return (
-            <Grid item xs={9} sx={{bgcolor: "#fff"}}>
-                <PokemonDetails data={data} />
+            <Grid item xs={12}>
+              <PokemonDetails data={data} />
             </Grid>
           );
         })}
-
-  
+   
     </Grid>
   );
 };
 
 export default Pokemon;
-
-
--- src/Components/PokemonDetails.jsx
+++ src/Components/PokemonDetails.jsx
@@ -1,61 +1,91 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import Divider from '@mui/material/Divider';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
+import { Typography } from "@mui/material";
 
 export const PokemonDetails = (data) => {
-
   return (
     <Grid container justifyContent="space-evenly" direction="row">
-
-      <Card >
-      <CardContent>
-        <Grid item xs={2}>
-        <Avatar src={data.data.sprites["front_default"]} />
-        </Grid>
-        <Grid item xs={10}>
-        <h3>{data.data.name}</h3>
-        </Grid>
-      </CardContent>
-      <Divider light />
-      <Box display={'flex'}>
-        <Box p={2} flex={'auto'} >
-          <p >Height</p>
-          <p >{data.data.height}</p>
+      <Card sx={{ textTransform: "capitalize" }}>
+        <CardContent>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-evenly"
+            alignItems={"center"}
+          >
+            <Grid item xs={3}>
+              <Typography variant="h3">{data.data.name}</Typography>
+              <Box p={1} flex={"auto"}>
+                {data.data.types.length > 0
+                  ? data.data.types.map((type, index) => {
+                      return (
+                        <Typography key={index} variant="p">
+                          {type.type.name}{" "}
+                          {index !== data.data.types.length - 1 ? " / " : ""}
+                        </Typography>
+                      );
+                    })
+                  : ""}
+              </Box>
+            </Grid>
+            <Grid item xs={3}>
+              <Avatar
+                alt=""
+                src={data.data.sprites["front_default"]}
+                sx={{ width: 150, height: 150 }}
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+        <Divider light />
+        <Box display={"flex"}>
+          <Box p={2} flex={"auto"}>
+            <p>{data.data.stats[0].stat.name}</p>
+            <p>{data.data.stats[0].base_stat}</p>
+          </Box>
+          <Box p={2} flex={"auto"}>
+            <p>{data.data.stats[1].stat.name}</p>
+            <p>{data.data.stats[1].base_stat}</p>
+          </Box>
+          <Box p={2} flex={"auto"}>
+            <p>{data.data.stats[2].stat.name}</p>
+            <p>{data.data.stats[2].base_stat}</p>
+          </Box>
+          <Box p={2} flex={"auto"}>
+            <p>{data.data.stats[3].stat.name}</p>
+            <p>{data.data.stats[3].base_stat}</p>
+          </Box>
+          <Box p={2} flex={"auto"}>
+            <p>{data.data.stats[4].stat.name}</p>
+            <p>{data.data.stats[4].base_stat}</p>
+          </Box>
+          <Box p={2} flex={"auto"}>
+            <p>{data.data.stats[5].stat.name}</p>
+            <p>{data.data.stats[5].base_stat}</p>
+          </Box>
         </Box>
-        <Box p={2} flex={'auto'} >
-          <p >Weight</p>
-          <p >{data.data.weight}</p>
+        <Divider light />
+        <Box display={"flex"}>
+          <Box p={2} flex={"auto"}>
+            <p>Height</p>
+            <p>{data.data.height}</p>
+          </Box>
+          <Box p={2} flex={"auto"}>
+            <p>Weight</p>
+            <p>{data.data.weight}</p>
+          </Box>
+          <Box p={2} flex={"auto"}>
+            <p>Experience</p>
+            <p>{data.data.base_experience}</p>
+          </Box>
         </Box>
-        <Box p={2} flex={'auto'} >
-          <p >Experience</p>
-          <p >{data.data.base_experience}</p>
-        </Box>
-      </Box>
-      <Box>
-      <h1>{data.data.name}</h1>
-      <img alt="" src={data.data.sprites["front_default"]} />
-      <ul>
-              <li>Name: {data.data.name}</li>
-              <li>ID: {data.data.id}</li>
-              <li>Base Experience: {data.data.base_experience}</li>
-              <li>Height: </li>
-              <li>Weight: </li>
-              <li>Types: {data.data.types.map((type) => type.type.name)}</li>
-              <li>Stats: {data.data.stats.map((stat) => stat.stat.name)}</li>
-              <li>Other Stats: {data.data.stats.map((stat) => stat.base_stat)}</li>
-              <li>Abilities: {data.data.abilities.map((ability) => ability.ability.name)}</li>
-            </ul>
-      </Box>
-    </Card>
+      </Card>
     </Grid>
   );
 };
-
-
-
-
